29a9b5a4465d5afb470fb5998a2e1dea41be3071
Sifat afroj moon <sifatafroj07@gmail.com>
2023-04-04 10:45:13 -0400

    Create APDM_Reader_nogt.m

---
+++ color-coding-core-and-minnesota-scan-statistics/color-coding/baseline-methods/EdgeLasso/APDM_Reader_nogt.m
@@ -0,0 +1,50 @@
+function [p_value,edges,label] = APDM_Reader_nogt(fileName)
+fid=fopen(fileName);
+tline = fgetl(fid);
+p_value=[]
+label=[]
+edges={}
+
+while ischar(tline)
+    
+    if strncmp(tline,'#',1)
+        tline = fgetl(fid);
+        continue;
+    elseif strstartswith(tline,'NodeID')
+        
+        while(true)
+            tline = fgetl(fid);
+            if ~strstartswith(tline,'END')
+                str=strsplit(' ',tline,'omit');
+                p_value=[p_value str2num(str{2})];
+            else
+                break
+            end            
+        end
+    elseif strstartswith(tline,'EndPoint0')
+         index=1;
+         while(true)
+            tline = fgetl(fid);
+            if ~strstartswith(tline,'END')
+                str=strsplit(' ',tline,'omit');
+                edges{index}(1)=str2num(str{1});
+                edges{index}(2)=str2num(str{2});
+                index=index+1;
+                
+            else
+                break
+            end            
+         end
+	 %fprintf('%d %d',edges{1}(1),edges{1}(2))
+	 %pause(10);
+    elseif strstartswith(tline, 'SECTION4')
+        break;
+    else
+        tline = fgetl(fid);
+        continue
+    %disp(tline)
+    
+    end
+end
+
+fclose(fid);
